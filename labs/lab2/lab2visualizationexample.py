c1901f4d67965a5a4285c2cd2f27998cea7b953e
Christopher Cortez <chris06.cortez@gmail.com>
2025-10-17 15:35:45 +0100

    partial lab work

---
+++ labs/lab2/lab2visualizationexample.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import bandas as pd
+import numpy as np
+
+# Artificial data for three classifiers
+np.random.seed(10)
+data = {
+        "Classifier A": np.array([0.75, 0.75, 0.74, 0.92, 0.69, 0.39, 0.85, 0.69, 0.79, 0.68]},
+
